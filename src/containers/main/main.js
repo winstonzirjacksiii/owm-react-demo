783875a1ba186e3e994968987213cc2ccec4f047
Winston Z <winston.zirjacks.iii+github@gmail.com>
2017-07-22 10:13:36 -0700

    started servering, added basic search component with console.logging of OWM data

--- src/containers/main/main.js
+++ src/containers/main/main.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import OpenApi from '../../modules/api';
+import SearchBar from '../../modules/main/search';
 
 class Main extends Component {
 	constructor () {
@@ -8,6 +9,36 @@ class Main extends Component {
 			activeQuery: '',
 			weatherData: []
 		}
+	}
+
+	searchForData(query) {
+		OpenApi.get(query).then((data)=>{
+			let updatedCities = this.state.weatherData;
+			updatedCities.push(data);
+
+			//prevent duplicate cities!
+
+			console.log("updatedCities", updatedCities)
+
+			this.setState({
+				weatherData: updatedCities,
+				activeQuery: query
+			});
+		});
+	}
+
+	render() {
+		return (
+			<div>
+				<section className='m-main_header'>
+					<SearchBar queryPlace={this.searchForData.bind(this)} />
+				</section>
+				<section className='m-main_content'>
+					<div className='m-main_widgets'>
+					</div>
+				</section>
+			</div>
+		)
 	}
 }
 
---
+++ src/modules/main/search.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+class SearchBar extends Component {
+	_handleSubmit(event) {
+		event.preventDefault();
+		this.props.queryPlace(this._queryString.value);
+		this._queryString = "";
+	}
+	
+	render() {
+		return (
+			<form onSubmit={this._handleSubmit.bind(this)}>
+				<input placeholder="City..." ref={ (c) => {this._queryString = c} } required />
+				<button type="submit">Search</button>
+			</form>
+		)
+	}
+}
+
+export default SearchBar;
